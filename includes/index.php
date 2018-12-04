2c43ccdbd67a569f81826af8ce5c1e778c4d56c6
Joanna Baile <joanna@studentsofferingsupport.org>
2018-12-04 00:24:03 -0500

    added about page; vue is working; setting up lightbox

--- includes/index.php
+++ includes/index.php
@@ -1,9 +1,9 @@
 <?php
     include 'functions.php';
 
-    //one work
-    if (isset($_GET["work"])) {
-        $data = get_one_work($conn, $_GET["work"]);
+    //returns the portfolio work
+    if (isset($_GET["port"])) {
+        $data = get_one_work($conn, $_GET["port"]);
         echo json_encode($data);
     } else {
         $data = get_all_work($conn);
