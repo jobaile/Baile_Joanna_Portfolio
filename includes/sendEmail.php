745fe6b8c81586489bdf91e2dd173e3c495d9574
Joanna Baile <joanna@studentsofferingsupport.org>
2018-12-09 17:05:51 -0500

    contact form working

--- includes/sendEmail.php
+++ includes/sendEmail.php
@@ -1,28 +1,29 @@
-<?php 
+<?php
 
 function send_email(){
 
-    if(empty($_POST['name'])
-       || empty($_POST['email'])
-       || empty($_POST['message'])
-       || empty($_POST['subject'])){
-            echo 'You are missing some required fields';
-            exit;
-        }
-    
-    $to = "joannabaile@gmail.com";
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    $formcontent = "From: $name \n Subject: $subject \n Message: $message";
-    $mailheader = "From: $email \r\n";
-    
-    mail($to, $subject, $formcontent, $mailheader);
-    
-    //header('Location: contact_complete.html');
-    exit;
+if(empty($_POST['name'])
+   || empty($_POST['email'])
+   || empty($_POST['message'])){
+
+        echo 'You are missing some required fields';
+        exit;
     }
-    
-    send_email();
+
+$to = "joannabaile@gmail.com";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$formcontent = "From: $name \n Subject: $subject \n Message: $message";
+$subject = "Portfolio Contact Form";
+$mailheader = "From: $email \r\n";
+
+mail($to, $subject, $formcontent, $mailheader);
+
+header('Location: sent.html');
+exit;
+}
+
+send_email();
 ?>
