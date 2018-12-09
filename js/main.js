4d0683c7b6e4c2f6e08fab67e68dbd83e3333bf9
Joanna Baile <joanna@studentsofferingsupport.org>
2018-12-09 07:51:05 -0500

    about page almost complete

--- js/main.js
+++ js/main.js
@@ -92,6 +92,49 @@
           video.webkitRequestFullscreen(); // Chrome and Safari
         }
       }
+
+      var skillBarStart = function(bars) {
+        this.bars = document.querySelectorAll(bars);
+        if( this.bars.length > 0 ) {
+          this.init();
+        }	
+      };
+      
+      skillBarStart.prototype = {
+        init: function() {
+          const self= this;
+          self.index = -1;
+          self.timer = setTimeout(function() {
+            self.action();
+          }, 400);
+          
+          
+        },
+        select: function( n ) {
+          const self= this,
+            bar = self.bars[n];
+            
+            if( bar ) {
+              var width = bar.parentNode.dataset.percent;
+            
+              bar.style.width = width;
+              bar.parentNode.classList.add( "complete" );	
+            }
+        },
+        action: function() {
+          const self= this;
+          self.index++;
+          if( self.index == self.bars.length ) {
+            clearTimeout( self.timer );
+          } else {
+            self.select( self.index );	
+          }
+          
+          setTimeout(function() {
+            self.action();
+          },400);
+        }
+      };
     
     hamburger.addEventListener("click", transformStart);
     playButton.addEventListener("click", playVideo); //VIDEO
@@ -100,6 +143,7 @@
     rewindVideo.addEventListener("click", rewindVid); //REWIND VIDEO X
     fullVideo.addEventListener("click", fullScreen); //FULLSCREEN
     video.addEventListener("timeupdate", progPlay); //TIME UPDATE
+    document.addEventListener("DOMContentLoaded", skillNewBars); // skill bar
   
 
 })();
