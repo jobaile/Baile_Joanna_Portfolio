b323a8e7d4c94b46c872dc77d86059a2420639a4
Joanna Baile <joanna@studentsofferingsupport.org>
2018-12-09 22:27:22 -0500

    new pics, new edits

--- js/vid.js
+++ js/vid.js
@@ -1,179 +1,78 @@
 (() => {
 
-// Video
-    var VideoPlayer = { 
-        init: function() {     
-          video.volume = .0;
-         
-          // Buttons
-          var playButton = document.querySelector("#play-pause");
-          var muteButton = document.querySelector("#mute");
-          var fullScreenButton = document.querySelector("#full-screen");
-          video = document.querySelector("#video");
-          
-          // Sliders
-          var seekBar = document.querySelector("#seek-bar");
-          var volumeBar = document.querySelector("#volume-bar");
-          volumeBar.value = video.volume;
-          
-          // Controls
-          videoControls = document.querySelector("#video-controls");
-          playCenterButton = document.querySelector("#video-play-center");
-          
-          // Event listener for the play/pause button 
-          VideoPlayer.hideControls(); 
+ //console.log("PureVolume website");
+ var video = document.querySelector("#headerVid");
+ var playButton = document.querySelector("#playPause");
+ var seekBar = document.querySelector("#seek-bar");
+ var rewindVideo = document.querySelector("#rewind");
+ var muteButton = document.querySelector("#muteVideo");
+ var volumeBar = document.querySelector('#volumeBar');
+ var fullVideo = document.querySelector("#full-screen");
 
+ function playVideo(){
+   console.log("from playVideo");
+   if (video.paused) {
+   document.getElementById("playPause").innerHTML='<button id="playPause"><i class="fas fa-pause"></i></button>';
+   video.play();
+ } else {
+   document.getElementById("playPause").innerHTML='<button id="playPause"><i class="fas fa-play"></i></button>';
+   video.pause();
+ }
+}
 
-          //display control on mousemove
-          document.querySelector("#video-container").addEventListener("mousemove", function() {   
-            VideoPlayer.showControls(); 
-          });
+ function progressBar(){
+   //console.log("from progressBar");
+   var time = video.duration * (seekBar.value / 100);
+   video.currentTime = time;
+ }
 
-          videoControls.addEventListener("mouseover", function() {   
-            VideoPlayer.showControls();
-            clearTimeout(hideControl);
-          });
+ function rewindVid(){
+   //console.log("from rewind");
+   video.currentTime -= 5;
+ }
 
-          playButton.addEventListener("click", function() {
-            //console.log('from playBtn');
-            playVideo();
-          });
+ function progPlay(){
+   console.log("from progPlay");
+   var value = (100 / video.duration) * video.currentTime;
+   seekBar.value = value;
+ }
 
-          playCenterButton.addEventListener("click", function() { 
-            //console.log('from playCenterBtn');
-            playVideo();
-          }); 
-          function playVideo(){ 
-            if (video.paused == true) {
-              // Play the video
-              video.play();
-              // Update the button text to 'Pause'
-              playButton.classList.toggle("fa-play");
-              playButton.classList.toggle("fa-pause");
-              playCenterButton.classList.toggle("fa-play-circle-o");
-              playCenterButton.classList.toggle("fa-pause");
-              playCenterButton.style.opacity = 0;
-            } else {
-              // Pause the video
-              video.pause();
-              // Update the button text to 'Play'
-              playButton.classList.toggle("fa-play");
-              playButton.classList.toggle("fa-pause");
-              playCenterButton.classList.toggle("fa-play-circle-o");
-              playCenterButton.classList.toggle("fa-pause");
-              playCenterButton.style.opacity = .8;
-            }
-          };
-  
-          // Checks if the document is currently in fullscreen mode
-          var isFullScreen = function() {
-            return !!(document.fullScreen || document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement || document.fullscreenElement);
-          }
-  
-          // Fullscreen
-          fullScreenButton.addEventListener("click", function() {
-            // If fullscreen mode is active...	
-            if (isFullScreen()) {
-              // ...exit fullscreen mode
-              if (document.exitFullscreen) document.exitFullscreen();
-              else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
-              else if (document.webkitCancelFullScreen) document.webkitCancelFullScreen();
-              else if (document.msExitFullscreen) document.msExitFullscreen();       
-            } else {
-              if (video.requestFullscreen) video.requestFullscreen();
-              else if (video.mozRequestFullScreen) video.mozRequestFullScreen();
-              else if (video.webkitRequestFullScreen) {
-                video.webkitRequestFullScreen();
-              } else if (video.msRequestFullscreen) video.msRequestFullscreen();
-              VideoPlayer.hideControls();         
-            }
-          });
-  
-  
-          // Event listener for the mute button
-          muteButton.addEventListener("click", function() {
-            if (video.muted == false) {
-              // Mute the video
-              video.muted = true;
-              // Update the volume Bar
-              volumeBar.value = 0;
-            } else {
-              // Unmute the video
-              video.muted = false;
-              // Update the volume Bar
-              volumeBar.value = video.volume;
-            }
-          });
-          
-          // Event listener for the seek bar
-          var seekBarActive = false;
-          seekBar.addEventListener("mousedown", function() {
-            seekBarActive = true;
-            clearTimeout(hideControl);
-          });
-          seekBar.addEventListener("mouseup", function() { 
-            seekBarActive = false;
-            VideoPlayer.hideControls(); 
-          });
-          seekBar.addEventListener("mousemove", function() {
-            if (seekBarActive == true){
-              // Calculate the new time
-              var time = video.duration * (seekBar.value / 100);
-              // Update the video time
-              video.currentTime = time;
-              clearTimeout(hideControl);
-            }
-          });
-          
-          // Update the seek bar as the video plays
-          video.addEventListener("timeupdate", function() {
-            // Calculate the slider value
-            var value = (100 / video.duration) * video.currentTime;
-            // Update the slider value
-            seekBar.value = value;
-          });
-          
-          // Event listener for the volume bar
-          var volumeBarActive = false;
-          volumeBar.addEventListener("mousedown", function() {
-            volumeBarActive = true;
-            clearTimeout(hideControl);
-          });
-          volumeBar.addEventListener("mouseup", function() { 
-            volumeBarActive = false;
-  
-          });
-          volumeBar.addEventListener("mousemove", function() {
-            if (volumeBarActive == true){
-              video.volume = volumeBar.value;
-              clearTimeout(hideControl);
-            }
-          });
-        },
-  
-        hideControls: function(){
-          hideControl = setTimeout(function(){ 
-            videoControls.style.opacity = .0;
-            playCenterButton.style.opacity = .0;
-            [].forEach.call(document.getElementsByClassName("controls"), function(el) {
-              el.style.visibility = "hidden";
-            });
-          }, 1000);  
-        },
-        showControls: function(){
-  
-          //Hide Display Control Bar
-          videoControls.style.opacity = .8;
-          playCenterButton.style.opacity = .8;
-          [].forEach.call(document.getElementsByClassName("controls"), function(el) {
-            el.style.visibility = "visible";
-          });
-          clearTimeout(hideControl);
-          VideoPlayer.hideControls();  
-        }
-      }
-      document.addEventListener('DOMContentLoaded', function () {
-        VideoPlayer.init();
-    });
+ function muteVideo(){
+   console.log("from muteVideo");
+   if (video.muted) {
+   video.muted = false;
+   document.getElementById("muteVideo").innerHTML='<button id="muteVideo" class="control"><i class="fas fa-volume-up"></i></button>';
+   theVolumeSVG.dataset.icon = "volume-up";
+   volumeBar.value = 1;
+   vidPlayer.volume = 1;
+   } else {
+   video.muted = true;
+   document.getElementById("muteVideo").innerHTML='<button id="muteVideo" class="control"><i class="fas fa-volume-off"></i></button>';
+   volumeBar.value = 0;
+   vidPlayer.volume = 0;
+   }
+ }
+
+ function changeVolume() {
+   vidPlayer.volume = volumeBar.value;
+ }
+
+ function fullScreen(){ //This will give default video controls
+   console.log("from fullScreen");
+   if (video.requestFullscreen) {
+     video.requestFullscreen();
+   } else if (video.mozRequestFullScreen) {
+     video.mozRequestFullScreen(); // Firefox
+   } else if (video.webkitRequestFullscreen) {
+     video.webkitRequestFullscreen(); // Chrome and Safari
+   }
+ }
+
+ playButton.addEventListener("click", playVideo); //VIDEO
+ seekBar.addEventListener("change", progressBar); //SEEK BAR
+ muteButton.addEventListener("click", muteVideo); //MUTE
+ rewindVideo.addEventListener("click", rewindVid); //REWIND VIDEO X
+ fullVideo.addEventListener("click", fullScreen); //FULLSCREEN
+ video.addEventListener("timeupdate", progPlay); //TIME UPDATE
 
 })();
